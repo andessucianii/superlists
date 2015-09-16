ea21bee44aec805fbf74fbdb101a35f222cc4f97
Andes Suciani <andessuciani12@gmail.com>
2015-09-16 07:34:22 +0000

    Menambahkan fitur komentar pribadi pada views, dan unit test

--- lists/views.py
+++ lists/views.py
@@ -6,5 +6,15 @@ def home_page(request):
 		Item.objects.create(text=request.POST['item_text'])
 		return redirect('/')
 	items = Item.objects.all()	
-	return render(request, 'home.html', {'items': items})
+	
+	item_pribadi = ''
+	if Item.objects.count() == 0:
+		item_pribadi = "yey, waktunya berlibur"
+	elif Item.objects.count() < 5:
+		item_pribadi = "sibuk tapi santai"
+	elif Item.objects.count() >= 5:
+		item_pribadi = "oh tidak"
+		
+
+	return render(request, 'home.html', {'items': items, 'item_pribadi' : item_pribadi})
 
