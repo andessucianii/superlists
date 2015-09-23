9e8689ab1c5149f9031f362a7f0c958033d14897
Andes Suciani <andessuciani12@gmail.com>
2015-09-23 06:14:45 +0000

    new url, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#import unittest
+
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -15,7 +15,7 @@ class NewVisitorTest(LiveServerTestCase):
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		#self.assertIn(row_text, [row.text for row in rows])
+		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get(self.live_server_url)
@@ -23,7 +23,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Hi! ini andes', self.browser.title)
 	
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Your to-do list', header_text)
+		self.assertIn('Start a new To-Do list', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
@@ -38,9 +38,9 @@ class NewVisitorTest(LiveServerTestCase):
 
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		#inputbox = self.browser.find_element_by_id('id_new_item')
-		#inputbox.send_keys('Use a peacock feathers to make a fly')
-		#inputbox.send_keys(Keys.ENTER)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
 		#self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
--- lists/tests.py
+++ lists/tests.py
@@ -66,7 +66,7 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
 	def test_home_page_only_saves_items_when_necessary(self):
@@ -74,15 +74,15 @@ class HomePageTest(TestCase):
 		home_page(request)
 		self.assertEqual(Item.objects.count(), 0)
 
-	def test_home_page_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+	#def test_home_page_displays_all_list_items(self):
+	#	Item.objects.create(text='itemey 1')
+	#	Item.objects.create(text='itemey 2')
 
-		request = HttpRequest()
-		response = home_page(request)
+	#	request = HttpRequest()
+	#	response = home_page(request)
 
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
+	#	self.assertIn('itemey 1', response.content.decode())
+	#	self.assertIn('itemey 2', response.content.decode())
 
 	#def test_fitur_komentar_zero(self):
 	#	request = HttpRequest()
@@ -114,3 +114,19 @@ class HomePageTest(TestCase):
 	#	self.assertGreaterEqual(Item.objects.count(), 5)
 	#	self.assertIn("oh tidak", response.content.decode())
 
+class ListViewTest(TestCase):
+
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+
+	def test_displays_all_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+
+		self.assertContains(response, 'itemey 1')
+		self.assertContains(response, 'itemey 2')
+
+	
